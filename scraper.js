37d6f6e866aad2462f54f88f058ac162d8afe846
FridaPolished <fridapulido@gmail.com>
2020-08-25 17:31:31 -0700

    Set web scraper basic functionality to extract data from Wikipedia

--- scraper.js
+++ scraper.js
@@ -1,16 +1,33 @@
 const puppeteer = require('puppeteer');
 
-async function scrapeWiki(url){
-  const browser = await puppeteer.launch();
-
+//working solution
+puppeteer.launch().then(async browser => {
   const page = await browser.newPage();
+  await page.goto('https://en.wikipedia.org/wiki/August_2');
+  const [el] = await page.$x('//*[@id="mw-content-text"]/div[1]/ul[1]');
+    const src = await el.getProperty('textContent');
+    const srcTxt = await src.jsonValue();
+    console.log({srcTxt});
+  await browser.close();
+});
 
-  page.goto(url);
 
-  const [el] = page.$x('//*[@id="mw-content-text"]/div[1]/ul[1]');
+//works but needs to extract values from the page
+// const url = 'https://en.wikipedia.org/wiki/August_2';
+// puppeteer
+// .launch()
+// .then(function(browser){
+//   return browser.newPage();
+// })
+// .then(function(page){
+//   return page.goto(url).then(function(){
+//     return page.content();
+//   })
+// })
+// .then(function(html){
+//   console.log(html)
+// })
+// .catch((err) => {
+//   console.log(err);
+// });
 
-  
-
-}
-
-scrapeWiki('https://en.wikipedia.org/wiki/August_2')
